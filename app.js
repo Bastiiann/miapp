f654dd5c53d39fe5187d6ae1a664e163e25c0f63
L1N0XZ <rip.linox@gmail.com>
2025-07-15 13:13:44 -0400

    Seq ID Deleter

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
+let contador = 0;
 const formulario = document.querySelector(".form-create");
-formulario.addEventListener("submit", (e)=>{
+formulario.addEventListener("submit", (e) => {
     e.preventDefault();
     const descripcion = document.querySelector("#descripcion").value;
     const contenedor = document.querySelector(".publicaciones");
     contenedor.innerHTML += ` <div class="publicacion">
-            <p>id: 00</p>
+            <p>id: ${contador}</p>
             <p>${descripcion}</p>
-            <button>Editar</button>
-            <button>Eliminar</button>
-        </div>`;
+            <button id="editar" onclick="editar(this)">editar</button>
+            <button id ="eliminar"onclick="eliminar(this)">eliminar</button>
+            </div>`;
+    ++contador;
+    const eliminar = document.querySelector("#eliminar");
+eliminar.addEventListener("click", (e) => {
+    console.log("se presiono click en el boton eliminar");
+    });
 });
+function editar(e){
+
+}
+function eliminar (e) {
+e.parentElemen.remove();
+}
