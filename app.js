4fb26eb4d9552ffdd13c441e8fc731e879b96476
Sethalexander2801 <sethmunoz@insucodos.cl>
2025-07-24 15:11:26 -0400

    add manejoarray.js

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
-let contador = 0;
 const formulario = document.querySelector(".form-create");
+let cont = 1;
 formulario.addEventListener("submit", (e) => {
     e.preventDefault();
     const descripcion = document.querySelector("#descripcion").value;
     const contenedor = document.querySelector(".publicaciones");
-    contenedor.innerHTML += ` <div class="publicacion">
-            <p>id: ${contador}</p>
+    contenedor.innerHTML += `<div id="${cont}" class="publicacion">
+            <p>id:${cont} </p>
             <p>${descripcion}</p>
-            <button id="editar" onclick="editar(this)">editar</button>
-            <button id ="eliminar"onclick="eliminar(this)">eliminar</button>
-            </div>`;
-    ++contador;
-    const eliminar = document.querySelector("#eliminar");
-eliminar.addEventListener("click", (e) => {
-    console.log("se presiono click en el boton eliminar");
-    });
+            <button id="editar" onclick="editar(this)">Editar</button>
+            <button id="eliminar" onclick="eliminar(this)">Eliminar</button>
+        </div>`;
+    cont++;
 });
-function editar(e){
-    const parrafo = e.parentElement.querySelector("p:nth-child(2)")
-    parrafo.contenteditable = true;
-    e.value = "guardar";
 
-    if (parrafo.contenteditable){
-    parrafo.contenteditable = false;
+
+function editar(e) {
+    const parrafo = e.parentElement.querySelector("p:nth-child(2)");
+    if (e.textContent == "Editar") {
+        parrafo.contentEditable = true;
+        e.textContent = "Guardar"
+        parrafo.focus();
     }
-    if(e.value =="guardar"){
-        e.value = "editar";
+    else {
+        e.textContent = "Editar"
+        parrafo.contentEditable = false;
     }
 }
-function eliminar (e) {
-e.parentElement.remove();
+
+function eliminar(e) {
+    e.parentElement.remove();
 }
